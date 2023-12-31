88c02be5e6f5e3db0531a655b3028293c98eabd6
Rodrigo Nicola <rodrigo.nicola0@gmail.com>
2023-12-31 18:32:08 -0300

    Add implementation for repeat func

--- 3-Iteration/repeat.go
+++ 3-Iteration/repeat.go
@@ -1,5 +1,8 @@
 package iteration
 
-func Repeat(character string) string {
-    return ""
+func Repeat(character string) (repeated string) {
+    for i := 0; i < 5; i++ {
+        repeated += character
+    }
+    return
 }
